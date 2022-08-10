151c4f506df962c53abb9da160600f32a9008144
Ingrid Cuadra <ingrid_cuadra@outlook.es>
2022-08-10 19:04:08 -0300

    fifth commit

--- js/userScript.js
+++ js/userScript.js
@@ -1,8 +1,22 @@
 // Registro del usuario
 
-function ingresarNick() {
-    let userNickname = prompt("Ingrese un nickname")
-    alert("¡Bienvenid@, " + userNickname + "!")
-}
+// function ingresarNick() {
+//     let userNickname = prompt("Ingrese un nickname")
+//     alert("¡Bienvenid@, " + userNickname + "!")
+// }
 
-ingresarNick()
+// ingresarNick()
+
+const form = document.getElementById("form")
+const userIngresado = document.getElementById("userIngresado")
+
+const usuarios = []
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault()
+    const userNickname = document.getElementById("userNickname").value
+    usuarios.push(userNickname)
+    console.log(userNickname)
+    form.reset()
+    userIngresado.innerHTML = `<p>¿List@ para jugar, ${userNickname}?</p>`
+})
